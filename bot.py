57efc6d9a2d9d83a305198a1864e50dcf5bf0f0f
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-13 04:19:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree. 
 '''
 
+# Made by Ayus Chatterjee
+
 import nltk
 import random
 import os
